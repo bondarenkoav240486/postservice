e6fa9d77ae2c2481734dad8212a6f84b63a4819c
bondarenkoav240486 <bondarenko.av@gmail.com>
2022-07-13 12:40:11 +0300

    Updates

---
+++ precache-manifest.1e1bb657e3f22e02a72977b34774648b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42b106300b3281efb13c",
+    "url": "/postservice/static/css/main.0a857464.chunk.css"
+  },
+  {
+    "revision": "42b106300b3281efb13c",
+    "url": "/postservice/static/js/main.42b10630.chunk.js"
+  },
+  {
+    "revision": "b0e02299b0352ff9107e",
+    "url": "/postservice/static/js/1.b0e02299.chunk.js"
+  },
+  {
+    "revision": "4ea262118e637f88ca42",
+    "url": "/postservice/static/js/runtime~main.4ea26211.js"
+  },
+  {
+    "revision": "41c125feeeebe19beaa94e3401446708",
+    "url": "/postservice/index.html"
+  }
+];
